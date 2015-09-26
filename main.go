1fea10b1ee53592a46332b461eccebe00bfd732a
James Bulmer <nekinie@gmail.com>
2015-09-26 08:08:08 +0100

    Drawing a blue box on screen

--- main.go
+++ main.go
@@ -1,78 +1,51 @@
 package main
 
 import (
+	"image"
+	"image/color"
+	"image/draw"
 	"log"
 
 	"golang.org/x/mobile/app"
 	"golang.org/x/mobile/event/lifecycle"
 	"golang.org/x/mobile/event/paint"
 	"golang.org/x/mobile/event/size"
-	"golang.org/x/mobile/exp/app/debug"
 	"golang.org/x/mobile/exp/gl/glutil"
+	"golang.org/x/mobile/geom"
 	"golang.org/x/mobile/gl"
 )
 
-const vertexShader = `#version 100
-uniform vec2 offset;
-attribute vec4 position;
-void main() {
-	// offset comes in with x/y values between 0 and 1.
-	// position bounds are -1 to 1.
-	vec4 offset4 = vec4(2.0*offset.x-1.0, 1.0-2.0*offset.y, 0, 0);
-	gl_Position = position + offset4;
-}`
-
-const fragmentShader = `#version 100
-precision mediump float;
-uniform vec4 color;
-void main() {
-	gl_FragColor = color;
-}`
-
 var (
-	images   *glutil.Images
-	fps      *debug.FPS
-	program  gl.Program
-	position gl.Attrib
-	offset   gl.Uniform
-	color    gl.Uniform
-	buf      gl.Buffer
+	images *glutil.Images
 )
 
 func onStart(glctx gl.Context) {
-	var err error
-	program, err = glutil.CreateProgram(glctx, vertexShader, fragmentShader)
-	if err != nil {
-		log.Printf("error creating GL program: %v", err)
-		return
-	}
-
-	buf = glctx.CreateBuffer()
-	glctx.BindBuffer(gl.ARRAY_BUFFER, buf)
-
 	images = glutil.NewImages(glctx)
-	fps = debug.NewFPS(images)
 }
 
 func onStop(glctx gl.Context) {
-	glctx.DeleteProgram(program)
-	glctx.DeleteBuffer(buf)
-	fps.Release()
 	images.Release()
 }
 
 func onPaint(glctx gl.Context, sz size.Event) {
+
 	glctx.ClearColor(128, 0, 128, 1)
 	glctx.Clear(gl.COLOR_BUFFER_BIT)
 
-	glctx.UseProgram(program)
+	m := images.NewImage(100, 100)
 
-	glctx.BindBuffer(gl.ARRAY_BUFFER, buf)
-	glctx.EnableVertexAttribArray(position)
+	draw.Draw(m.RGBA, m.RGBA.Bounds(), &image.Uniform{color.RGBA{0, 0, 255, 255}}, image.Point{}, draw.Src)
 
-	glctx.DisableVertexAttribArray(position)
+	m.Upload()
 
-	fps.Draw(sz)
+	m.Draw(
+		sz,
+		geom.Point{0, 0},
+		geom.Point{100, 0},
+		geom.Point{0, 100},
+		m.RGBA.Bounds(),
+	)
+
 }
 
 func main() {
